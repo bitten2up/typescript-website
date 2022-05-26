80b17109acc221d07647061869afb753662a0dc2
bitten 1up <seant9140@gmail.com>
2022-05-26 15:50:48 -0500

    "bitten" + "2up"

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
-function greeter(person) {
-    return "Hello, " + person;
-  }
-   
-  let user = "bitten2up";
-   
-  document.body.textContent = greeter(user);
+interface Person {
+  firstName: string;
+  lastName: string;
+}
+interface app {
+  username: string;
+  id: BigInt;
+}
+function greeter(person: Person) {
+  return "Hello, " + person.firstName + " " + person.lastName;
+}
+function generate(app: App){
+  return `<style>*{color:#94bfa7;margin: 0;}</style><p>loged in as: " + app.username + app.id + "</p><canvas id="myCanvas" width="200" height="100"></canvas><script src="scripts/canvas.js"></script>`
+}
+function appSetup(){
+  let app = {username: prompt("enter a username"), id: 000001}
+}
+let user = { firstName: "typescript", lastName: "website" };
+document.body.textContent = greeter(user);
