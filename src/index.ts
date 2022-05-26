d3d9741c98faf009ecaeae352d3e7aaf49b60a07
bitten 1up <seant9140@gmail.com>
2022-05-26 16:02:17 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ interface Person {
   firstName: string;
   lastName: string;
 }
-interface app {
+interface App {
   username: string;
-  id: BigInt;
+  id: string;
 }
 function greeter(person: Person) {
   return "Hello, " + person.firstName + " " + person.lastName;
@@ -13,7 +13,7 @@ function generate(app: App){
   return `<style>*{color:#94bfa7;margin: 0;}</style><p>loged in as: " + app.username + app.id + "</p><canvas id="myCanvas" width="200" height="100"></canvas><script src="scripts/canvas.js"></script>`
 }
 function appSetup(){
-  let app = {username: prompt("enter a username"), id: 000001}
+  let app = {username: prompt("enter a username"), id: "000001"}
 }
 let user = { firstName: "typescript", lastName: "website" };
 document.body.textContent = greeter(user);
